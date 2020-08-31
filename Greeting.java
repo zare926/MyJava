bee49fafa8b0ffc564937c5b31a8f5611971a409
zare926 <ilove.boc926@gmail.com>
2020-08-31 21:45:58 +0900

    JavaLesson1

---
+++ Greeting.java
@@ -0,0 +1,7 @@
+class Greeting{
+  public static void main(String[] args){
+    System.out.println("Good mornign");
+    System.out.println("Good afternoon");
+    System.out.println("Good evening");
+  }
+}
